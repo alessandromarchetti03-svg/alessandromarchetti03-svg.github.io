348656a4134ccb32bfd43eb879dcdb71f6864c79
alessandromarchetti03-svg <alessandro.marchetti03@gmail.com>
2026-08-27 06:37:31 +0200

    Add files via upload

--- gestione-mm26/service-worker.js
+++ gestione-mm26/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "preventivi-emme2000-v5";
+const CACHE = "preventivi-emme2000-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
